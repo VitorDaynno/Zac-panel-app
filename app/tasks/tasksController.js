733a3c9fbf727ae3863d263dad78f326701c14f0
VitorDaynno <vitordaynno@gmail.com>
2019-05-06 23:25:08 -0300

    Alterações na exibição

--- app/tasks/tasksController.js
+++ app/tasks/tasksController.js
@@ -47,24 +47,6 @@
             })
         }
 
-        vm.parseDate = (dateStr) =>{
-            const date = new Date(dateStr);
-            const day = date.getDate() + 1;
-            let month = date.getMonth() + 1;
-            month = month < 9 ? '0' + month : month;
-            const year = date.getFullYear();
-            return day + '/' + month + '/' + year;
-        }
-
-        vm.parseHour = (dateStr) => {
-            const date = new Date(dateStr);
-            let hour = date.getHours();
-            hour = hour > 9 ? hour : '0' + hour;
-            let minutes = date.getMinutes();
-            minutes = minutes > 9 ? minutes : '0' + minutes;
-            return hour + ':' + minutes
-        }
-
         vm.getTasks();
     }
 })()
